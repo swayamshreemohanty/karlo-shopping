df4d1659bfbed417e49ea06eef5d4817ef472cee
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-12 13:46:51 +0530

    unused package removed

--- functions/index.js
+++ functions/index.js
@@ -31,6 +31,36 @@ class User{
     }
 }
 
+// class Product{
+// 	constructor(productId,productImageUrl,productName,productPrice,productDescription){
+//     this.productId=productId;
+//     this.productImageUrl=productImageUrl;
+//     this.productName=productName;
+//     this.productPrice=productPrice;
+//     this.productDescription=productDescription;
+//     }
+
+//     fromMap(map) {
+//         return new Product(
+//             map['productId']??"",
+//             map['productImageUrl']??"",
+//             map['productName']??"",
+//             map['productPrice']??"",
+//             map['productDescription']??"",
+//         );
+//       }
+
+//     toMap(){
+//         return{
+//             "productId":this.productId,
+//             "productImageUrl":this.productImageUrl,
+//             "productName":this.productName,
+//             "productPrice":this.productPrice,
+//             "productDescription":this.productDescription,
+//         }
+//     }
+// }
+
 exports.userAdded= functions.auth.user().onCreate(async user=>{
     console.log('Adding user');
     var newUser=new User().fromMap(user);
@@ -46,3 +76,28 @@ exports.userDeleted= functions.auth.user().onDelete(async user=>{
     await firestore.collection('user').doc(deletedUser.uid).delete();
     return Promise.resolve()
 });
+
+
+// exports.addItem = functions.https.onRequest(async(request,response)=>{
+//     try {
+//         switch (request.method) {
+           
+//             case 'POST':
+//                 const body= request.body;
+//                 let product=new Product.fromMap(body)
+//                 console.log(product)
+//                 let collectionDoc=firestore.collection('AllItems');
+//                 product.productId=collectionDoc.id;
+//                 await collectionDoc.doc(product.productId).set()
+//                 response.statusCode(200).send('message:"${product.productName} added to cart"');
+//                 break;
+          
+//             default:
+//                 response.statusCode(400).send('No method found');
+//                 break;
+//         }
+//     } catch (error) {
+//         console.log(error)
+//         response.status(400).send('Error occured');          
+//     }
+// })
