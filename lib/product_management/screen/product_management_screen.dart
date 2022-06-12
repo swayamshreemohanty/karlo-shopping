e145f38311cfafce14494899fa0e665c6fcd4999
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-12 15:25:32 +0530

    code updated

--- lib/product_management/screen/product_management_screen.dart
+++ lib/product_management/screen/product_management_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:shopping_app/model/product_model.dart';
+import 'package:shopping_app/product_management/logic/product_management/productmanagement_bloc.dart';
 import 'package:shopping_app/product_management/widgets/product_management_dialog_widget.dart';
+import 'package:shopping_app/product_management/widgets/product_showcase_tile.dart';
+import 'package:shopping_app/utility/loading_indicator.dart';
 
 class ProductManagementScreen extends StatefulWidget {
   const ProductManagementScreen({Key? key}) : super(key: key);
@@ -19,13 +25,42 @@ class _ProductManagementScreenState extends State<ProductManagementScreen> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (ctx) => const ProductManageDialog(),
-            );
-          }),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (ctx) => const ProductManageDialog(),
+          );
+        },
+      ),
+      body: StreamBuilder<List<ProductModel?>>(
+        stream: context.read<ProductmanagementBloc>().fetchProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingIndicator();
+          } else {
+            final logs = snapshot.data ?? [];
+            if (logs.isEmpty) {
+              return const Center(
+                child: Text("No products found."),
+              );
+            } else {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: logs.length,
+                itemBuilder: (context, index) =>
+                    AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  child: ScaleAnimation(
+                    child: ProductShowCaseTile(product: logs[index]!),
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
