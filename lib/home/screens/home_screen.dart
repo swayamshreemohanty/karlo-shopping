8431a5562c63e3c2749c91bed7b0936b9548bda6
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-12 10:32:59 +0530

    user role fetcher added on home screen

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app/home/repository/user_role.dart';
 import 'package:shopping_app/home/widgets/app_drawer.dart';
 import 'package:shopping_app/utility/firebase_current_user_data.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    context.read<UserRoleRepository>().fetchuserRole();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
