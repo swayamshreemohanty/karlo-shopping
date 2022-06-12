0de0e68385fd08e21a7af9034b10469c4d3c5783
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-12 18:16:55 +0530

    model updated

--- lib/cart/model/cart_product_model.dart
+++ lib/cart/model/cart_product_model.dart
@@ -1,35 +1,63 @@
 import 'package:shopping_app/model/product_model.dart';
 
-class CartProductModel {
+class CartProductSummary {
   String? orderId;
-  int quantity;
-  ProductModel product;
+  List<CartProductModel> cartProductsList;
   DateTime? orderCreatedAt;
-  CartProductModel({
+  String totalPrice;
+
+  CartProductSummary({
     this.orderId,
-    required this.product,
-    required this.quantity,
     this.orderCreatedAt,
+    this.totalPrice = "0",
+    required this.cartProductsList,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'orderId': orderId,
+      'cartProductsList': cartProductsList.map((x) => x.toMap()).toList(),
+      'orderCreatedAt': orderCreatedAt?.millisecondsSinceEpoch,
+      'totalPrice': totalPrice,
+    };
+  }
+
+  factory CartProductSummary.fromMap(Map<String, dynamic> map) {
+    return CartProductSummary(
+      orderId: map['orderId'] != null ? map['orderId'] as String : null,
+      cartProductsList: List<CartProductModel>.from(
+        (map['cartProductsList'] as List<int>).map<CartProductModel>(
+          (x) => CartProductModel.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+      orderCreatedAt: map['orderCreatedAt'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(
+              (map['orderCreatedAt'] ?? 0) as int)
+          : null,
+      totalPrice: (map['totalPrice'] ?? '') as String,
+    );
+  }
+}
+
+class CartProductModel {
+  int quantity;
+  ProductModel product;
+  CartProductModel({
+    required this.quantity,
+    required this.product,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
       'quantity': quantity,
       'product': product.toMap(),
-      'orderCreatedAt': orderCreatedAt?.millisecondsSinceEpoch,
     };
   }
 
   factory CartProductModel.fromMap(Map<String, dynamic> map) {
     return CartProductModel(
-      orderId: map['orderId'] != null ? map['orderId'] as String : null,
-      quantity: (map['quantity'] ?? 1) as int,
+      quantity: (map['quantity'] ?? 0) as int,
       product: ProductModel.fromMap(map['product'] as Map<String, dynamic>),
-      orderCreatedAt: map['orderCreatedAt'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(
-              (map['orderCreatedAt'] ?? 0) as int)
-          : null,
     );
   }
 }
