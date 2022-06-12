a9ab2d4bbf8f733394d2edafd739032db41d13a4
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-12 09:50:05 +0530

    device orientation set to portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopping_app/authentication/logic/authentication/google_authentication_bloc.dart';
@@ -12,6 +13,9 @@ import 'package:shopping_app/home/screens/home_screen.dart';
 import 'package:shopping_app/utility/loading_screen.dart';
 
 Future<void> main() async {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
